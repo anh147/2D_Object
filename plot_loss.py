6e280cb7711be54d5a3e2d37a2b844067360f6c6
anh147 <147anhnt@gmail.com>
2022-12-18 02:18:18 +0700

    a

---
+++ plot_loss.py
@@ -0,0 +1,6 @@
+import matplotlib
+import numpy as np
+
+measure = np.array([[30.4, 7.4], [29.8, 7.1], [19.6,10.5], [29.4,5.8], [27.6,8.5]])
+predict = np.array([[30.472, 6.97], [29.87, 6.86], [19.63,9.93], [29.59,5.33], [27.72,8.16]])
+print(measure)
